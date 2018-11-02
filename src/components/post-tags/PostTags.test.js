d79b24b57fda7c252727dc338d7d801abb1447c3
Diyana Dimitrova <diyana@riseart.com>
2018-11-02 11:22:52 +0200

    Better tests

--- src/components/post-tags/PostTags.test.js
+++ src/components/post-tags/PostTags.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { PostTags } from "./PostTags";
 import { shallow } from "enzyme";
-import { Button } from "antd";
 
 describe("PostTags Component", () => {
   it("render tag label", () => {
@@ -9,16 +8,4 @@ describe("PostTags Component", () => {
     const tagLabel = <div className="label">Tags:</div>;
     expect(wrapper.contains(tagLabel)).toBeTruthy();
   });
-  // it("render tags", () => {
-  //   const wrapper = shallow(<PostTags tags={["test"]} />);
-
-  //   const tag = (
-  //     <Button className="tag" size="small">
-  //       test
-  //     </Button>
-  //   );
-
-  //   console.log("wrapper", wrapper.html());
-  //   expect(wrapper.contains(tag)).toBeTruthy();
-  // });
 });
